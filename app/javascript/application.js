82371f4ff9bddd5c0772dbc22ef2d88ac70ed123
emily martinek <emily.martinek12@gmail.com>
2023-01-25 16:24:12 +0200

    add navbar component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require semantic-ui
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from "jquery";
+window.jQuery = jquery
+window.$ = jquery
+
+$(document).on('turbo:load', function() {
+    console.log('loaded turbo links')
+    $('.ui.dropdown').dropdown()
+});
